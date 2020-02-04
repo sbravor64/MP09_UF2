fe187cca17682c4ccba081dd145abcc5d7e567b2
sbravor64 <Andresbravo97>
2020-02-04 17:25:41 +0100

    hecho

---
+++ src/com/company/e7/Atleta.java
@@ -0,0 +1,18 @@
+package com.company.e7;
+
+class Atleta extends Thread {
+    Testimoni testimoni;
+
+    public Atleta(Testimoni testimoni) {
+        this.testimoni = testimoni;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
+
+class Testimoni {
+
+}
